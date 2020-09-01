ce4d99c0a04f09944a48ac8e472dfecff91db7eb
Rodrigo Daza <rodrigoads12@gmail.com>
2020-09-01 18:49:18 -0400

    Login y Register conectados a Firebase

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { User } from 'firebase';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -18,38 +19,69 @@ export class RegisterPage implements OnInit {
       apellidoApoderado: '',
       password: ''
   };
+  
+  public user : User;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(public alertController: AlertController,
+              private authService: AuthService, 
+              private router: Router) { }
 
   ngOnInit() {
   }
 
   async registrarCuenta(){
     try {
-      var nombreAlumno = this.juntarNombre(this.usuarioRegistro.nombreApoderado,this.usuarioRegistro.apellidoApoderado);
-      var nombreApoderado = this.juntarNombre(this.usuarioRegistro.nombreApoderado,this.usuarioRegistro.apellidoApoderado)
+      var nombreAlumno = this.juntarNombre(this.usuarioRegistro.nombreApoderado, this.usuarioRegistro.apellidoApoderado);
+      var nombreApoderado = this.juntarNombre(this.usuarioRegistro.nombreApoderado, this.usuarioRegistro.apellidoApoderado)
       const user = await this.authService.register(this.usuarioRegistro.email, this.usuarioRegistro.password, nombreAlumno, nombreApoderado);
       if (user) {
+        this.router.navigate(['/tablinks']);
         console.log(user);
+      }else{
+        this.alertaCuenta();
+        //this.router.navigate(['/login']);
       }
     } catch (error) {
       console.log(error);
     }
   }
 
-  private checkUserIsVerified(user: User) {
-    if (user) {
-      this.router.navigate(['/tablinks']);
-    } else {
-      this.router.navigate(['/register']);
-    }
+  async alertaCuenta() {
+
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      //subHeader: 'Subtitle',
+      message: 'Este correo ya fue registrado',
+      //buttons: ['Salir']
+      buttons: [
+        {
+          text: "Salir",
+          handler: () => {
+            this.router.navigate(['/login']);
+          }
+        }
+      ]
+    });
+    
+    await alert.present();
+    
   }
+
+
+  // private checkUserIsVerified(user: User) {
+  //   if (user) {
+  //     this.router.navigate(['/tablinks']);
+  //   } else {
+  //     this.router.navigate(['/register']);
+  //   }
+  // }
 
   juntarNombre(nombre: string, apellido: string){
     
     const espacio = " ";
-    var nombreCompleto =  nombre.toUpperCase().concat(espacio.toString());
-    nombreCompleto = nombreCompleto.concat(apellido.toUpperCase().toString());
+    var nombreCompleto =  nombre.concat(espacio.toString());
+    nombreCompleto = nombreCompleto.concat(apellido.toString());
     return nombreCompleto;
   }
 
