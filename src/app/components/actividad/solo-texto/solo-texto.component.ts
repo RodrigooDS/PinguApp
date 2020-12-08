400c37f3abc66cf21cf7ce87761abd7cc7f0b692
Sebastián Solar <ssolar@talana.com>
2020-12-08 20:42:38 -0300

    Actividad solo-texto avance

--- src/app/components/actividad/solo-texto/solo-texto.component.ts
+++ src/app/components/actividad/solo-texto/solo-texto.component.ts
@@ -13,83 +13,48 @@ export class SoloTextoComponent implements OnInit {
   @Input() tituloCategoria: string;
   @Input() actividadContenido: string;
 
-
   seleccionRadioButton: boolean;
-
-  actividad: any[] = [];
-  pregunta: string;
+  data: any[] = [];
+  posicion: number = 0;
+  respuestas : any[] = [];
+  respuestasCorrecta : any[] = [];
   preguntas: any[] = [];
-
-  respuestas:  any[] = [];
-  answers: any[] = [];
-  
-  respuesta_correcta: any[]=[];
-
-  opcion: number;                 //opción que marca el usuario.
+  opcion: number;                 
   incorrectas: number [] = [];    
 
   constructor(public obtener_actividades: ObtenerActivadesService) { }
 
-  ngOnInit() {
-    this.obtenerPreguntas();
-    this.obtenerRespuestas();
+  async ngOnInit() {
+    await this.obtenerDatosActividad();
+    await this.obtenerRespuestas();
   }
 
+  //Se obtienen los valores de las actividades.
   async obtenerDatosActividad(){
-    this.actividad = await this.obtener_actividades.obtenerActividad(this.tituloActividad,this.tituloCategoria);
-    return this.actividad;
+    this.data = await this.obtener_actividades.obtenerActividad(this.tituloActividad,this.tituloCategoria);
   }
-
-  async obtenerPreguntas() {
-
-    this.actividad = await this.obtenerDatosActividad();
-    this.preguntas = new Array(this.actividad.length);
-
-    for(let i=0; i < this.preguntas.length; i++){
-      this.preguntas[i] =  this.actividad[i].pregunta;
-    }
-
-  };
 
   async obtenerRespuestas(){
-
-    let respuesta = await this.obtenerDatosActividad();
-
-    for(let j = 0; j < this.preguntas.length; j++){
-      for (let i = 0; i < 4; i++){
-        this.respuestas.push(respuesta[j].respuestas[i]);
-      }            
+    for(let i=0; i< this.data.length;i++){
+        this.respuestas.push(this.data[i].respuestas);
+        this.respuestasCorrecta.push(this.data[i].correcta);
+        this.preguntas.push(this.data[i].pregunta);
     }
-
-    for(let i=0; i<this.preguntas.length;i++){
-
-      this.respuesta_correcta.push(respuesta[i].correcta);
-
-      if(i==0){
-        this.answers[i] = (this.respuestas.slice(0 , 4));  
-      }
-      else{
-        let x = 7;
-        this.answers[i] = (this.respuestas.slice(i*(x-3), (x*i)+1));
-      }
-    }
-
-   console.log(this.preguntas)
-   console.log(this.answers);
-   console.log(this.respuesta_correcta);
-
   }
 
   obtenerRespuetaCheckBox(i: number) {
     this.opcion = i;
-    console.log(this.opcion)
   }
 
   enviarDatos(){
-    if(this.respuesta_correcta[0] == this.opcion){
-      console.log("Respuesta Correcta");
+    if(this.respuestasCorrecta[this.posicion] == this.opcion){
+      // console.log("Respuesta Correcta");
+      this.posicion++;
+      console.log(this.posicion);
+      // console.log(this.incorrectas);
+      this.incorrectas.splice(0,this.incorrectas.length);
     } else {
-      console.log("Respuesta Incorrecta");
+      // console.log("Respuesta Incorrecta");
       this.incorrectas.push(this.opcion);
     }
   };
